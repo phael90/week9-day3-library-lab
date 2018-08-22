716e5fb924363e4d23f0c501fc65af0f60eb92e8
Raphael Ugha <raphaelugha@gmail.com>
2018-08-22 14:13:20 +0100

    Created class Borrower

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Borrower {
+    private int id;
+    private String name;
+    private Set<Book> books;
+
+    public Borrower(String name){
+        this.name = name;
+        this.books = ArrayList<Book>;
+    }
+
+    public Borrower(){}
+
+
 }
